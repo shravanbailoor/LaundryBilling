011c96f41adfc10d6f7762a1f3097e70d9201a85
shravanbailoor <42926612+shravanbailoor@users.noreply.github.com>
2018-09-13 00:12:06 +0530

    1. Basic Send SMS functionality implemented

--- app/src/main/java/com/example/laundrybilling/OrderData.java
+++ app/src/main/java/com/example/laundrybilling/OrderData.java
@@ -1,8 +1,17 @@
 package com.example.laundrybilling;
 
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class OrderData extends AppCompatActivity {
 
@@ -13,6 +22,74 @@ public class OrderData extends AppCompatActivity {
     }
 
     public void onClickPrint(View view) {
+        sendSMS("5554","This is a test message");
+    }
+
+    //Send SMS functionality - Start - 12092018
+    private void sendSMS(String phoneNumber, String message) {
+        ArrayList<PendingIntent> sentPendingIntents = new ArrayList<PendingIntent>();
+        ArrayList<PendingIntent> deliveredPendingIntents = new ArrayList<PendingIntent>();
+        PendingIntent sentPI = PendingIntent.getBroadcast(this, 0,
+                new Intent(OrderData.this, SmsSentReceiver.class), 0);
+        PendingIntent deliveredPI = PendingIntent.getBroadcast(this, 0,
+                new Intent(OrderData.this, SmsDeliveredReceiver.class), 0);
+        try {
+            SmsManager sms = SmsManager.getDefault();
+            ArrayList<String> mSMSMessage = sms.divideMessage(message);
+            for (int i = 0; i < mSMSMessage.size(); i++) {
+                sentPendingIntents.add(i, sentPI);
+                deliveredPendingIntents.add(i, deliveredPI);
+            }
+            sms.sendMultipartTextMessage(phoneNumber, null, mSMSMessage,
+                    sentPendingIntents, deliveredPendingIntents);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getBaseContext(), "SMS sending failed...",Toast.LENGTH_SHORT).show();
+        }
 
     }
+
+    public class SmsDeliveredReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent arg1) {
+            switch (getResultCode()) {
+                case Activity.RESULT_OK:
+                    Toast.makeText(OrderData.this, "SMS delivered", Toast.LENGTH_SHORT).show();
+                    break;
+                case Activity.RESULT_CANCELED:
+                    Toast.makeText(OrderData.this, "SMS not delivered", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    }
+
+    public class SmsSentReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent arg1) {
+            switch (getResultCode()) {
+                case Activity.RESULT_OK:
+                    Toast.makeText(OrderData.this, "SMS Sent", Toast.LENGTH_SHORT).show();
+
+                    break;
+                case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                    Toast.makeText(OrderData.this, "SMS generic failure", Toast.LENGTH_SHORT)
+                            .show();
+
+                    break;
+                case SmsManager.RESULT_ERROR_NO_SERVICE:
+                    Toast.makeText(OrderData.this, "SMS no service", Toast.LENGTH_SHORT)
+                            .show();
+
+                    break;
+                case SmsManager.RESULT_ERROR_NULL_PDU:
+                    Toast.makeText(OrderData.this, "SMS null PDU", Toast.LENGTH_SHORT).show();
+                    break;
+                case SmsManager.RESULT_ERROR_RADIO_OFF:
+                    Toast.makeText(OrderData.this, "SMS radio off", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    }
+    //Send SMS functionality - End - 12092018
 }
